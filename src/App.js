5408900588d4b29d9bb9ca4f323fdd44ac3f9bcd
unknown <warnerak@dukes.jmu.edu>
2019-06-27 17:42:09 -0400

    working product - want to refine project more

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ImageCard from "./components/ImageCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import cards from "./cards.json";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.cards to the cards json array
+  state = {
+    cards,
+    score: 0,
+    highscore: 0
+  };
+
+  gameOver = () => {
+    // Changes the highscore to the current score, if said score is larger than the higherscore
+    if (this.state.score > this.state.highscore) {
+      this.setState({ highscore: this.state.score }, function () {
+        console.log(this.state.highscore);
+      });
+    }
+    // Reset all card counts to zero and run the alert that you lost the game
+    this.state.cards.forEach(card => {
+      card.count = 0;
+    });
+    alert(`Game Over :( \nscore: ${this.state.score}`);
+    this.setState({ score: 0 });
+    return true;
+  }
+
+  clickCount = id => {
+    //loops through the cards to check to see if the card has been clicked
+    //if card count is clicked then it sets the count to 1 and shuffle the cars
+    //if clicked again then it will run the game over function
+    this.state.cards.find((o, i) => {
+      if (o.id === id) {
+        if (cards[i].count === 0) {
+          cards[i].count = cards[i].count + 1;
+          this.setState({ score: this.state.score + 1 }, function () {
+            console.log(this.state.score);
+          });
+          this.state.cards.sort(() => Math.random() - 0.5)
+          return true;
+        } else {
+          this.gameOver();
+        }
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <Title score={this.state.score} highscore={this.state.highscore}>Clicky Game</Title>
+        {this.state.cards.map(card => (
+          <ImageCard
+            clickCount={this.clickCount}
+            id={card.id}
+            key={card.id}
+            image={card.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
